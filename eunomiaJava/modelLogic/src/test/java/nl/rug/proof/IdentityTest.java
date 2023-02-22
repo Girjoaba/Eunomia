46ff140eab670d7ea70c1a639a896205fe6d29da
Andrei <a.girjoaba@student.rug.nl>
2023-02-22 19:57:26 +0100

    unit testing scaffolding

--- eunomiaJava/modelLogic/src/test/java/nl/rug/proof/IdentityTest.java
+++ eunomiaJava/modelLogic/src/test/java/nl/rug/proof/IdentityTest.java
@@ -7,8 +7,118 @@ import nl.rug.proof.lineVerification.LineVerifier;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
+@Slf4j
 public class IdentityTest {
+    @Test
+    public void testIdentityIntro() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile("testProofs/identity/correct/identity_intro.txt");
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void testIdentityElim() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile("testProofs/identity/correct/identity_elim.txt");
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void testIdentityElimBinary() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile("testProofs/identity/correct/identity_elim_binary.txt");
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void testIdentityIntroWrong() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile("testProofs/identity/wrong/identity_intro_wrong.txt");
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void testIdentityElimWrong() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile("testProofs/identity/wrong/identity_elim_wrong.txt");
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void testIdentityElimOrderWrong() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile("testProofs/identity/wrong/identity_elim_order_wrong.txt");
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void testIdentityElimSideWrong() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile("testProofs/identity/wrong/identity_elim_side_wrong.txt");
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void testIdentityElimBinaryWrong() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile("testProofs/identity/wrong/identity_elim_binary_wrong.txt");
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
 }

336b803106da27179d453264d27c94de87b7959f
Andrei <a.girjoaba@student.rug.nl>
2023-04-19 19:23:50 +0200

    Created Unit Tests for Universal Quantifiers

--- eunomiaJava/modelLogic/src/test/java/nl/rug/proof/quantifier/ForallTest.java
+++ eunomiaJava/modelLogic/src/test/java/nl/rug/proof/quantifier/ForallTest.java
@@ -13,6 +13,12 @@ import java.util.List;
 
 @Slf4j
 public class ForallTest {
+
+    /*  ----------------------------------------------------------------------------------------------------------------
+        ------------------------------------------------- Forall Elimination -------------------------------------------
+        ----------------------------------------------------------------------------------------------------------------
+     */
+
     @Test
     public void testForallElim() {
         try {
@@ -42,7 +48,67 @@ public class ForallTest {
     }
 
     @Test
-    public void testForallElimWrongNotCompleted() {
+    public void forallElimDifferentConstantsTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_elim_different_constants.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(2));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void forallElimNotFunctionTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_elim_not_function.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(2));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void forallElimReplaceWrongVariableTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_elim_replace_wrong_variable.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(2));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void forallElimSwitchVariableTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_elim_switch_variable.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(2));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void forallElimWrongNotCompleteTest() {
         try {
             ProofManager manager = new ProofManager();
             EunomiaCompiler compiler = new EunomiaCompiler(manager);
@@ -57,13 +123,122 @@ public class ForallTest {
     }
 
     @Test
-    public void testForallElimWrongToVariable() {
+    public void ForallElimWrongToVariableTest() {
         try {
             ProofManager manager = new ProofManager();
             EunomiaCompiler compiler = new EunomiaCompiler(manager);
             compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_elim_wrong_to_variable.txt"));
 
             List<Integer> wrongLines = new ArrayList<>(List.of(2));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    /*  ----------------------------------------------------------------------------------------------------------------
+        ------------------------------------------------- Forall Introduction ------------------------------------------
+        ----------------------------------------------------------------------------------------------------------------
+     */
+
+    @Test
+    public void forallIntroCorrectSimpleTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/correct/forall_intro_correct_simple.txt"));
+
+            LineVerifier.verifyAllLinesCorrect(manager);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void ForallIntroChangedFunctionTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_intro_changed_function.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(4));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void ForallIntroExistentScopeTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_intro_existent_scope.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(3, 4));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void ForallIntroNoQuantifierInferredTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_intro_no_quantifier_inferred.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(4));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void ForallIntroNotAllConstantsReplacedTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_intro_not_all_constants_replaced.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(4));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void ForallIntroNotMatchingNotConstantTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_intro_not_matching_constant.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(4));
+            LineVerifier.verifyWrongLines(manager, wrongLines);
+
+        } catch (FileNotFoundException e) {
+            log.error("File not found");
+        }
+    }
+
+    @Test
+    public void ForallIntroSameVariableAsExistentTest() {
+        try {
+            ProofManager manager = new ProofManager();
+            EunomiaCompiler compiler = new EunomiaCompiler(manager);
+            compiler.compile(new InputPath("testProofs/quantifiers/forall/wrong/forall_intro_same_variable_as_existent.txt"));
+
+            List<Integer> wrongLines = new ArrayList<>(List.of(4));
             LineVerifier.verifyWrongLines(manager, wrongLines);
 
         } catch (FileNotFoundException e) {
